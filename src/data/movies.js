bcf82b9c08f58136bd740fc585e375c3123e2b30
RomikSm <roma.semen.com@gmail.com>
2025-05-06 16:59:14 +0300

    add movies.js

---
+++ src/data/movies.js
@@ -0,0 +1,74 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    genre: "Sci-Fi, Action",
+    poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
+    trailer: "https://www.youtube.com/embed/YoHD9XEInc0",
+    date: "2023-03-20",
+    time: "18:00",
+    duration: "2h 28min",
+    rating: 8.8
+  },
+  {
+    id: 2,
+    title: "The Shawshank Redemption",
+    description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    genre: "Drama",
+    poster: "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+    trailer: "https://www.youtube.com/embed/6hB3S9bIaco",
+    date: "2023-03-21",
+    time: "19:30",
+    duration: "2h 22min",
+    rating: 9.3
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    genre: "Action, Crime, Drama",
+    poster: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+    trailer: "https://www.youtube.com/embed/EXeTwQWrcwY",
+    date: "2023-03-22",
+    time: "20:00",
+    duration: "2h 32min",
+    rating: 9.0
+  },
+  {
+    id: 4,
+    title: "Pulp Fiction",
+    description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    genre: "Crime, Drama",
+    poster: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    trailer: "https://www.youtube.com/embed/s7EdQ4FqbhY",
+    date: "2023-03-23",
+    time: "21:15",
+    duration: "2h 34min",
+    rating: 8.9
+  },
+  {
+    id: 5,
+    title: "Forrest Gump",
+    description: "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+    genre: "Drama, Romance",
+    poster: "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+    trailer: "https://www.youtube.com/embed/bLvqoHBptjg",
+    date: "2023-03-24",
+    time: "17:45",
+    duration: "2h 22min",
+    rating: 8.8
+  },
+  {
+    id: 6,
+    title: "The Matrix",
+    description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+    genre: "Action, Sci-Fi",
+    poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+    trailer: "https://www.youtube.com/embed/vKQi3bBA1y8",
+    date: "2023-03-25",
+    time: "19:00",
+    duration: "2h 16min",
+    rating: 8.7
+  }
+];
